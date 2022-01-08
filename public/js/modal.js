8e1e3911b9d37aba0435fceb88eb2d8b45f6fbc5
69safe <endokun32@yahoo.co.jp>
2022-01-09 08:01:03 +0900

    モーダル画面をChromeでも表示されるように修正

---
+++ public/js/modal.js
@@ -0,0 +1,28 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*******************************!*\
+  !*** ./resources/js/modal.js ***!
+  \*******************************/
+$(function () {
+  // 変数に要素を入れる
+  var open = $('.modal-open'),
+      close = $('.modal-close'),
+      container = $('.modal-container'); //開くボタンをクリックしたらモーダルを表示する
+
+  open.on('click', function () {
+    container.addClass('active');
+    return false;
+  }); //閉じるボタンをクリックしたらモーダルを閉じる
+
+  close.on('click', function () {
+    container.removeClass('active');
+  }); //モーダルの外側をクリックしたらモーダルを閉じる
+
+  $(document).on('click', function (e) {
+    if (!$(e.target).closest('.modal-body').length) {
+      container.removeClass('active');
+    }
+  });
+});
+/******/ })()
+;
