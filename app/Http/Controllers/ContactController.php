6ad49eb425eba5692946f151d10e4d793be05fbb
69safe <endokun32@yahoo.co.jp>
2022-01-10 18:14:55 +0900

    CSVインポート機能を実装

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,8 +6,6 @@ use Illuminate\Http\Request;
 use App\Models\Contact;
 use App\Imports\CsvImport;
 use Symfony\Component\HttpFoundation\StreamedResponse;
-use Maatwebsite\Excel\Facades\Excel;
-use Maatwebsite\Excel\Validators\ValidationException;
 
 class ContactController extends Controller
 {
@@ -30,9 +28,6 @@ class ContactController extends Controller
 
             $stream = fopen('php://output','w');
             $contact = new Contact();
-
-            // 文字化け回避
-            stream_filter_prepend($stream, 'convert.iconv.utf-8/cp932//TRANSLIT');
 
             // ヘッダー行を追加
             fputcsv($stream, $contact->csvHeader());
@@ -58,13 +53,38 @@ class ContactController extends Controller
 
     public function csvImport(Request $request) {
 
-        $file = $request->file();
+        $filePath = $request->file('file');
+        $file = new \SplFileObject($filePath);
+        $file->setFlags(
+            \SplFileObject::READ_CSV | // CSVとして行を読み込み
+            \SplFileObject::READ_AHEAD | // 先読み／巻き戻しで読み込み
+            \SplFileObject::SKIP_EMPTY | // 空行を読み飛ばす
+            \SplFileObject::DROP_NEW_LINE // 行末の改行を読み飛ばす
+        );
 
-        try {
-            $import = new CsvImport();
-            Excel::import($import, $file);
-        } catch (ValidationException $e) {
-            Log::alert($e->errors());
+        $i = 0;
+
+        // 一行ずつ処理
+        foreach($file as $line)
+        {
+            if ($i != 0) {
+                $contact = new Contact();
+                $contact->condition_id = $line[0];
+                $contact->design_id = $line[1];
+                $contact->email = $line[2];
+                $contact->tel_number = $line[3];
+                $contact->fax_number = $line[4];
+                $contact->zipcode = $line[5];
+                $contact->pref = $line[6];
+                $contact->city = $line[7];
+                $contact->street = $line[8];
+                $contact->surname = $line[9];
+                $contact->name = $line[10];
+                $contact->memo = $line[11];
+                $contact->private_memo = $line[12];
+                $contact->save();
+            }
+            $i++;
         }
 
         return redirect('/');
